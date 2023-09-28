8d7ec8c26fa6adccf24c740c4d079b2625cd614b
souravmohanta <souravmohanta19@gmail.com>
2023-09-28 00:43:03 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+
+const menuList = document.getElementById("menu");
+const hamburgerBtn = document.getElementById("hamburger");
+
+document.onclick = function (e) {
+
+  if (e.target.id !== "menu" && e.target.id !== "hamburger") {
+    hamburgerBtn.classList.remove("active");
+    menuList.classList.remove("active");
+  }
+};
+
+
+hamburgerBtn.onclick = function () {
+  hamburgerBtn.classList.toggle("active");
+  menuList.classList.toggle("active");
+};
+
+const modal = document.querySelector(".modal");
+const previews = document.querySelectorAll(".img-gallery img");
+const fullImage = document.querySelector(".full-img");
+const caption = document.querySelector(".caption");
+const closeBtn = document.querySelector(".close-btn");
+
+previews.forEach((preview) => {
+  preview.addEventListener("click", () => {
+    modal.classList.add("open");
+    fullImage.classList.add("open");
+   
+    const originalSrc = preview.getAttribute("src");
+    fullImage.src = originalSrc;
+
+    const altText = preview.alt;
+    caption.textContent = altText;
+  });
+});
+
+modal.addEventListener("click", (e) => {
+  if (e.target.classList.contains("modal")) {
+    modal.classList.remove("open");
+    original.classList.remove("open");
+  }
+});
+
+closeBtn.addEventListener("click", (e) => {
+  if (e.target.classList.contains("close-btn")) {
+    modal.classList.remove("open");
+    original.classList.remove("open");
+  }
+});
